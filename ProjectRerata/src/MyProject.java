2977fa888d6380979fde72fe3156c3188cc3e4fe
Aqil Sadik <dmadaaqil@gmail.com>
2021-11-09 18:03:04 +0700

    Create MyProject.java

---
+++ ProjectRerata/src/MyProject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class MyProject {
+
+    public static void main(String[] args) {
+        ArrayProcessing array = new ArrayProcessing();
+        array.input();
+        array.output();
+        array.urutkan();
+    }
+    
+}
